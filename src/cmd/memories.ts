2440b3e3d5ec5e77624fa5855c23f68c65cd65a2
Luis Andres Villegas <luis.villegas@unisimon.edu.co>
2020-05-12 21:12:25 -0500

    avances en el comando 'memories'

---
+++ src/cmd/memories.ts
@@ -0,0 +1,26 @@
+import command from '../commands'
+import { Client, MessageEmbed } from 'discord.js';
+import { SnkNames } from '../util/snknames';
+
+export default class Memories extends command {
+
+    constructor() {
+        super(['memories'], '', false)
+    }
+
+    call(client: Client, msg: any): void {
+
+        const memories = '`2000`'
+        const character = new SnkNames().randomCharacter(200, 5);
+        const embed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Recuerdos')
+            .addFields(
+                { name: '⚔ Personaje', value: character['name'], inline: true },
+                { name: '📔 Recuerdos', value: memories, inline: true }
+            )
+
+        msg.channel.send(embed);
+    }
+
+}
