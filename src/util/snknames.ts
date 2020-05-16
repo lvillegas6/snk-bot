60243ab7a0fcb0e3a193c641aed74ca492f8ef11
zlToxicNetherlz <abrahamlora3@gmail.com>
2020-05-16 16:11:50 -0500

    se agregaron nuevos parametros a personajes genericos

--- src/util/snknames.ts
+++ src/util/snknames.ts
@@ -13,13 +13,17 @@ export class SnkNames {
         if (random.integer(1, 2) === 2) {
             return {
                 genre: 'female',
-                name: fakerator.names.nameF()
+                name: fakerator.names.nameF(),
+                age: random.integer(16, 32),
+                image: 'https://i.imgur.com/ePkQqCb.png'
             }
         }
 
         return {
             genre: 'male',
-            name: fakerator.names.nameM()
+            name: fakerator.names.nameM(),
+            age: random.integer(16, 32),
+            image: 'https://i.imgur.com/ePkQqCb.png'
         }
     }
 
@@ -29,6 +33,6 @@ export class SnkNames {
         if (memories >= 200 && random.integer(1, 100) >= (90 - plus)) {
             return this.shingekiCharacter()
         }
-        return this.shingekiCharacter()
+        return this.genericCharacter()
     }
 }
