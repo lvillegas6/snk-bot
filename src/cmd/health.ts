394659e1b8b828b98ef7dbdd24e6b674d987eaf0
Luis Andres Villegas <luis.villegas@unisimon.edu.co>
2020-06-09 13:07:46 -0500

    Se cambio el código a 2 espacios

--- src/cmd/health.ts
+++ src/cmd/health.ts
@@ -8,31 +8,31 @@ import command from '../commands';
 
 export default class Health extends command {
 
-    constructor() {
-        super(['health'], '', false)
-    }
+  constructor() {
+    super(['health'], '', false)
+  }
 
-    call(client: Client, msg: any): void {
+  call(client: Client, msg: any): void {
 
-        const player: SnkPlayer = database.getSoftPlayer(msg.author.id, msg.guild.id);
+    const player: SnkPlayer = database.getSoftPlayer(msg.author.id, msg.guild.id);
 
-        if (!player.hasBody()) {
+    if (!player.hasBody()) {
 
-            database.getPlayerManager(msg.guild.id).getPlayers();
-            SnkDefaults.sendNotSoulMessage(msg);
-            return;
-
-        }
-
-        const embed = new MessageEmbed()
-            .setColor('#f75e2f')
-            .setAuthor(msg.author.username, msg.author.avatarURL())
-            .setTitle('Tu Salud')
-            .setDescription(`Actualmente posees una salud de \`${player.getHealth()}\`.`)
-            .setTimestamp();
-
-        msg.channel.send(embed);
+      database.getPlayerManager(msg.guild.id).getPlayers();
+      SnkDefaults.sendNotSoulMessage(msg);
+      return;
 
     }
+
+    const embed = new MessageEmbed()
+      .setColor('#f75e2f')
+      .setAuthor(msg.author.username, msg.author.avatarURL())
+      .setTitle('Tu Salud')
+      .setDescription(`Actualmente posees una salud de \`${player.getHealth()}\`.`)
+      .setTimestamp();
+
+    msg.channel.send(embed);
+
+  }
 
 }
