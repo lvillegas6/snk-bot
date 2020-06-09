dd4cdf31a75e6c5984bf58ecaafb64acd7caad59
zlToxicNetherlz <abrahamlora3@gmail.com>
2020-06-09 12:18:28 -0500

    comandos de energía y salud

---
+++ src/cmd/energy.ts
@@ -0,0 +1,38 @@
+import { SnkPlayer } from '../system/database';
+import { SnkDefaults } from '../system/defaults';
+import { Client, MessageEmbed } from 'discord.js';
+
+import { database } from '../main';
+import command from '../commands';
+
+export default class Energy extends command {
+
+  constructor() {
+    super(['energy'], '', false)
+  }
+
+  call(client: Client, msg: any): void {
+
+    let player: SnkPlayer = database.getSoftPlayer(msg.author.id, msg.guild.id);
+
+    if (!player.hasBody()) {
+
+      database.getPlayerManager(msg.guild.id).getPlayers();
+      SnkDefaults.sendNotSoulMessage(msg);
+      return;
+
+    }
+
+    let embed = new MessageEmbed()
+      .setColor('#19fa68')
+      .setAuthor(msg.author.username, msg.author.avatarURL())
+      .setTitle('Tu Energía')
+      .setDescription("Actualmente posees una energía de `" + player.getEnergy() + "`.")
+      .setTimestamp();
+
+    msg.channel.send(embed);
+    return;
+
+  }
+
+}
